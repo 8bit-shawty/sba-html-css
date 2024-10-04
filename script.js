1a08c6d97f224387e5b3a885e620659f0f8a0f86
MF <mfregwork21@gmail.com>
2024-10-03 17:15:37 -0700

    Working Email Validation

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-console.log("hey")
+const loginForm = document.getElementById("loginForm")
+const registerForm = document.getElementById("registerForm")
+
+const errMessageEl = document.getElementById('errorDisplay')
+
+const email = loginForm.elements['email']
+const password = loginForm.elements['password']
+const firstName = registerForm.elements['firstName']
+const lastName = registerForm.elements['lastName']
+
+loginForm.addEventListener('submit', validateLogin)
+
+function validateLogin(e){
+    e.preventDefault();
+    const emailVal = validateEmail();
+    if(emailVal === false){
+        return
+    }
+    // alert(`Email: ${email.value}`)
+    // return true;
+}
+
+function validateEmail() {
+    let emailVal = email.value.trim();
+
+    if(emailVal === ''){
+        return showErrMessage("The email must not be blank")
+    }
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailPattern.test(emailVal)){
+        return showErrMessage("Please enter a valid email.")
+    }
+
+    return true
+}
+
+
+
+function showErrMessage(msg){
+    errMessageEl.textContent = msg;
+    errMessageEl.style.display = 'block'
+    errMessageEl.style.marginTop = '25px'
+
+    setTimeout(() => {
+        errMessageEl.style.display = 'none'
+    }, 4000);
+}
